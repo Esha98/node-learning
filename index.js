ceba6f01ffde01ea0d84b378801042a38aebd4aa
Esha98 <eshashah3335@gmail.com>
2022-04-15 17:38:08 +1000

    Tried custom-middleware and learn how middleware works

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express');
 const App = express();
 const PORT = 3000;
 
-App.get('/about', function (req, res) {
+const customMiddleware = (req, res, next) => {
+  console.log('Middleware calling');
+  next();
+};
+
+App.use(customMiddleware);
+
+App.get('/', function (req, res) {
   res.send('About page');
 });
 
 App.listen(PORT, () => {
-  console.log('app is running at ${PORT}');
+  console.log('app is running at', PORT);
 });
